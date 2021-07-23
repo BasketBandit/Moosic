99c6f9f5912298fd5110314bdf37cdd4d1498642
BasketBandit <errorsfound@hotmail.co.jp>
2021-07-23 17:24:23 +0100

    init spring boot default setup

---
+++ src/test/java/com/basketbandit/moosic/MoosicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basketbandit.moosic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoosicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
