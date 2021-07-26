bffa950ccbdaa8be9e75ee2d96bc8d9283fb33b4
BasketBandit <errorsfound@hotmail.co.jp>
2021-07-26 17:07:21 +0100

    update /load mapping to queue an audio track via url

--- src/main/java/com/basketbandit/moosic/MoosicApplication.java
+++ src/main/java/com/basketbandit/moosic/MoosicApplication.java
@@ -1,23 +1,45 @@
 package com.basketbandit.moosic;
 
+import com.basketbandit.moosic.player.LavaPlayer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @SpringBootApplication
 @RestController
 public class MoosicApplication {
+	private static final Logger log = LoggerFactory.getLogger(MoosicApplication.class);
+	private LavaPlayer player = new LavaPlayer();
+
 	public static void main(String[] args) {
 		SpringApplication.run(MoosicApplication.class, args);
 	}
 
 	@GetMapping("/")
-	public ModelAndView root(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public ModelAndView root() {
 		ModelAndView modelAndView = new ModelAndView("index");
-		modelAndView.addObject("name", name);
+		return modelAndView;
+	}
+
+	@PostMapping("/load")
+	public RedirectView load(@RequestParam(value = "url") String url) {
+		if(url != null) {
+			player.getAudioLoadHandler().load(url);
+		}
+		RedirectView redirectView = new RedirectView("/");
+		return redirectView;
+	}
+
+	@GetMapping("/dashboard")
+	public ModelAndView dashboard() {
+		ModelAndView modelAndView = new ModelAndView("dashboard");
 		return modelAndView;
 	}
 }
