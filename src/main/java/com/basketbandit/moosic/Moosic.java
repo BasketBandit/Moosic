cbb1a642695168b2e25fbf64a9647eac9d5dfd13
BasketBandit <errorsfound@hotmail.co.jp>
2021-08-06 10:28:14 +0100

    refactor MoosicApplication to simply Moosic

---
+++ src/main/java/com/basketbandit/moosic/Moosic.java
@@ -0,0 +1,11 @@
+package com.basketbandit.moosic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moosic {
+	public static void main(String[] args) {
+		SpringApplication.run(Moosic.class, args);
+	}
+}
